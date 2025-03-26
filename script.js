10c8088c2f3652beec39c8434813428a0db28475
Peter Abahim <lilpero03@gmail.com>
2025-03-26 01:23:59 +0100

    first push

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// 26 MARCH 2025 PRACTICE AT HOME
+
+// ARITHEMTIC OPERATOR
+
+let a = 20
+let b = 5
+let c = 60
+let d = 45
+
+console.log(a+c);
+console.log(c-d);
+console.log(c/b);
+console.log(a*b);
+console.log(d%a);
+console.log(b**a);
+
+
+// ASSIGNMENT OPERATION
+
+// a/=b
+// console.log(a);
+
+// c*=a
+// console.log(c);
+
+// a**=b
+// console.log(a);
+
+
+// COMPARISON OPERATOR
+
+// let a = 10
+// let b = 2
+// let c = 80
+// let d = 35
+// let e = 2
+
+// console.log(b==e);
+// console.log(b!=e);
+// console.log(a===e);
+// console.log(c>e);
+// console.log(d<c);
+// console.log(b>=d);
+// console.log(e<=a);
+// console.log(d!==b);
+
+
+// INCREAMENT AND DECREAMENT OPERATOR
+
+// let a = 5
+// let b = 2
+// let c = 8
+
+// c++
+// console.log(c);
+
+// a--
+// console.log(a);
+
+// console.log((++b)+(--c));
+// console.log((b++)+(c--));
+// console.log((b++)+(--c));
+// console.log((++b)+(c--));
