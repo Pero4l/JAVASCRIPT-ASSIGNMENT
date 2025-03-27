ff67447074a3d38059424d409219e70a7c65097c
Peter Abahim <lilpero03@gmail.com>
2025-03-28 00:14:02 +0100

    i add another assignment of 27 march to it

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 
 // ARITHEMTIC OPERATOR
 
-let a = 20
-let b = 5
-let c = 60
-let d = 45
+// let a = 20
+// let b = 5
+// let c = 60
+// let d = 45
 
-console.log(a+c);
-console.log(c-d);
-console.log(c/b);
-console.log(a*b);
-console.log(d%a);
-console.log(b**a);
+// console.log(a+c);
+// console.log(c-d);
+// console.log(c/b);
+// console.log(a*b);
+// console.log(d%a);
+// console.log(b**a);
 
 
 // ASSIGNMENT OPERATION
@@ -62,3 +62,90 @@ console.log(b**a);
 // console.log((b++)+(c--));
 // console.log((b++)+(--c));
 // console.log((++b)+(c--));
+
+
+
+// 27 MARCH 2025 PRACTICE AT HOME
+
+
+let a = 5
+let b = 10
+let c = 20
+let d = 13
+let e = 5
+let f = 13
+let g = 14
+
+// Q1. IS  A EQUAL TO B?
+console.log(a==b);
+
+
+// Q2. IS  A*B EQUAL TO D?
+console.log((a*b)==d);
+
+
+// Q3. IS  A%B EQUAL TO C%B?
+console.log((a%b)==c%b);
+
+
+// Q4. IS  D GREATER THAN F?
+console.log(d>f);
+
+
+// Q5. IS  A*B+C EQUAL TO G*5?
+console.log((a*b+c) == g*5);
+
+
+// Q6. WHAT WILL BE THE OUTPUT OF (2 + 3) * (4 - 1)
+console.log((2+3) * (4-1));
+
+
+// Q7. WHAT WILL BE THE OUTOUT OF 5=="5"
+console.log(5=="5");
+
+
+// Q8. WRITE A PROGRAM THAT DECLARES TWO NUMBERS AND PRINT THEIR SUM, DIFFERENCE, PRODUCT, QUOTIENT AND REMAINDER
+
+let number1 = 50
+let number2 = 10
+
+console.log(
+    "Sum:", number1+number2
+);
+
+console.log(
+    "Difference:", number1-number2
+);
+
+console.log(
+    "Product:", number1*number2
+);
+
+console.log(
+    "Quotient:", number1/number2
+);
+
+console.log(
+    "Remainder:", number1%number2
+);
+
+
+
+// Q9.  WRITE A PROGRAM THAT TAKES A NUMBERS AND PRINT SQUARE OF THAT NUMBER
+
+let num = 7
+let square = num ** 2
+
+console.log("Square:", square);
+
+
+
+
+//  Q10. WRITE A PROGRAM THAT TAKES A NUMBERS AND PRINT SQUARE ROOT OF THAT NUMBER
+
+
+
+let root = 20
+let squareRoot = Math.sqrt(20)
+
+console.log("SquareRoot:", squareRoot);
