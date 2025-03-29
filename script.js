33390a7410fc64fda897b1f6998039674513a334
Peter Abahim <lilpero03@gmail.com>
2025-03-29 20:53:38 +0100

    I add another new assignment on if and else conditional statement

--- script.js
+++ script.js
@@ -68,84 +68,171 @@
 // 27 MARCH 2025 PRACTICE AT HOME
 
 
-let a = 5
-let b = 10
-let c = 20
-let d = 13
-let e = 5
-let f = 13
-let g = 14
+// let a = 5
+// let b = 10
+// let c = 20
+// let d = 13
+// let e = 5
+// let f = 13
+// let g = 14
 
 // Q1. IS  A EQUAL TO B?
-console.log(a==b);
+// console.log(a==b);
 
 
 // Q2. IS  A*B EQUAL TO D?
-console.log((a*b)==d);
+// console.log((a*b)==d);
 
 
 // Q3. IS  A%B EQUAL TO C%B?
-console.log((a%b)==c%b);
+// console.log((a%b)==c%b);
 
 
 // Q4. IS  D GREATER THAN F?
-console.log(d>f);
+// console.log(d>f);
 
 
 // Q5. IS  A*B+C EQUAL TO G*5?
-console.log((a*b+c) == g*5);
+// console.log((a*b+c) == g*5);
 
 
 // Q6. WHAT WILL BE THE OUTPUT OF (2 + 3) * (4 - 1)
-console.log((2+3) * (4-1));
+// console.log((2+3) * (4-1));
 
 
 // Q7. WHAT WILL BE THE OUTOUT OF 5=="5"
-console.log(5=="5");
+// console.log(5=="5");
 
 
 // Q8. WRITE A PROGRAM THAT DECLARES TWO NUMBERS AND PRINT THEIR SUM, DIFFERENCE, PRODUCT, QUOTIENT AND REMAINDER
+// let number1 = 50
+// let number2 = 10
 
-let number1 = 50
-let number2 = 10
+// console.log(
+//     "Sum:", number1+number2
+// );
 
-console.log(
-    "Sum:", number1+number2
-);
+// console.log(
+//     "Difference:", number1-number2
+// );
 
-console.log(
-    "Difference:", number1-number2
-);
+// console.log(
+//     "Product:", number1*number2
+// );
 
-console.log(
-    "Product:", number1*number2
-);
+// console.log(
+//     "Quotient:", number1/number2
+// );
 
-console.log(
-    "Quotient:", number1/number2
-);
-
-console.log(
-    "Remainder:", number1%number2
-);
+// console.log(
+//     "Remainder:", number1%number2
+// );
 
 
 
 // Q9.  WRITE A PROGRAM THAT TAKES A NUMBERS AND PRINT SQUARE OF THAT NUMBER
+// let num = 7
+// let square = num ** 2
 
-let num = 7
-let square = num ** 2
-
-console.log("Square:", square);
+// console.log("Square:", square);
 
 
 
 
 //  Q10. WRITE A PROGRAM THAT TAKES A NUMBERS AND PRINT SQUARE ROOT OF THAT NUMBER
+// let root = 20
+// let squareRoot = Math.sqrt(20)
+
+// console.log("SquareRoot:", squareRoot);
 
 
 
-let root = 20
-let squareRoot = Math.sqrt(20)
+// TODAY MARCH 29, I ADDED ANOTHER ASSIGNMENT ON IF AND ELES STATEMENT
 
-console.log("SquareRoot:", squareRoot);
+
+
+// SCORES OF A GAME
+
+
+let score = 1000
+let grade
+
+if(score>=700 && score<=1000){
+    grade = "A"
+}
+
+else if(score>=500 && score<=690){
+    grade = "B"
+}
+
+else if(score>=400 && score<=590){
+    grade = "C"
+}
+
+else if(score>=300 && score<=490){
+    grade = "D"
+}
+
+else{
+    grade = "F"
+}
+
+
+console.log(grade);
+
+
+// GREETING
+
+
+let time = 6
+let greeting
+
+if(time>=0 && time<=12 ){
+        greeting = "Good Morning"
+}
+else if(time>=12 && time<=18){
+        greeting = "Good Afternoon"
+}
+else{
+    greeting = "Good Evening"
+}
+
+console.log(greeting);
+
+
+
+
+
+// FESTIVE SEASION WISHES
+
+
+let day = 25
+let dec = "december"
+let wish
+
+if(day== 25 && dec== "december"){
+    wish = "Merry Christmas"
+}
+
+else {
+    wish = " "
+}
+
+console.log(wish);
+
+
+// BIRTHDAY WISHES
+
+let birthDay = 7
+let month = "March"
+let birthdayWish
+
+if(birthDay == 7 && month == "March"){
+    birthdayWish = "Happy Birthday Dear Customer"
+}
+
+else{
+    " "
+}
+
+console.log(birthdayWish);
